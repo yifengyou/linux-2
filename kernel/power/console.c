aab94f356bdc70d23a8032f805e369d699674081
Amit Kucheria <amit.kucheria@ubuntu.com>
2008-05-23 11:43:45 +0300

    UBUNTU: SAUCE: pm: Config option to disable handling of console during suspend/resume
    Signed-off-by: Amit Kucheria <amit.kucheria@ubuntu.com>
    
    Signed-off-by: Ben Collins <ben.collins@canonical.com>

--- kernel/power/console.c
+++ kernel/power/console.c
@@ -17,20 +17,24 @@ static int orig_fgconsole, orig_kmsg;
 
 int pm_prepare_console(void)
 {
+#ifndef CONFIG_PM_DISABLE_CONSOLE
 	orig_fgconsole = vt_move_to_console(SUSPEND_CONSOLE, 1);
 	if (orig_fgconsole < 0)
 		return 1;
 
 	orig_kmsg = kmsg_redirect;
 	kmsg_redirect = SUSPEND_CONSOLE;
+#endif
 	return 0;
 }
 
 void pm_restore_console(void)
 {
+#ifndef CONFIG_PM_DISABLE_CONSOLE
 	if (orig_fgconsole >= 0) {
 		vt_move_to_console(orig_fgconsole, 0);
 		kmsg_redirect = orig_kmsg;
 	}
+#endif
 }
 #endif
