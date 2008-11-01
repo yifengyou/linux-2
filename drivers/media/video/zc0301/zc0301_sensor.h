e6628d75fe7c7b2a6797fc56b786838c7df8b10b
Jani Monoses <jani@ubuntu.com>
2008-11-01 13:37:33 +0200

    UBUNTU: Fix webcam having USB ID 0ac8:303b
    Bug: #292086
    
    Make webcams with USB ID 0ac8:303b, such as the Z-Star Microelectronics
    Corp. ZC0303 be handled by gspca not the zc0301 driver. The latter does
    not seem to handle them anymore in Intrepid.
    
    Signed-off-by: Jani Monoses <jani@ubuntu.com>
    Signed-off-by: Andy Whitcroft <apw@canonical.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>

--- drivers/media/video/zc0301/zc0301_sensor.h
+++ drivers/media/video/zc0301/zc0301_sensor.h
@@ -62,7 +62,6 @@ zc0301_attach_sensor(struct zc0301_device* cam, struct zc0301_sensor* sensor);
 #define ZC0301_ID_TABLE                                                       \
 static const struct usb_device_id zc0301_id_table[] =  {                      \
 	{ ZC0301_USB_DEVICE(0x046d, 0x08ae, 0xff), }, /* PAS202 */            \
-	{ ZC0301_USB_DEVICE(0x0ac8, 0x303b, 0xff), }, /* PB-0330 */           \
 	{ }                                                                   \
 };
 #else
