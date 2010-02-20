bfb76d17c48f643c5fd6f5204465fe564da1474d
Loïc Minier <loic.minier@ubuntu.com>
2010-02-20 05:28:53 +0000

    UBUNTU: SAUCE: [um] Don't use nx_enabled under UML
    This fixes our NX support patch when building for UML which isn't
    upstream.
    
    BugLink: http://bugs.launchpad.net/bugs/524849
    
    Signed-off-by: Loïc Minier <loic.minier@ubuntu.com>
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- fs/binfmt_elf.c
+++ fs/binfmt_elf.c
@@ -716,7 +716,7 @@ static int load_elf_binary(struct linux_binprm *bprm, struct pt_regs *regs)
 	if (retval)
 		goto out_free_dentry;
 
-#ifdef CONFIG_X86_32
+#if defined(CONFIG_X86_32) && !defined(CONFIG_UML)
 	/*
 	 * Turn off the CS limit completely if NX active:
 	 */
