abf2d2f8629e63c1dcdce4c7d3529cdaf59ab948
Andy Whitcroft <apw@canonical.com>
2010-03-06 11:36:40 +0000

    UBUNTU: armel -- perf userspace does not support arm
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- debian.master/rules.d/armel.mk
+++ debian.master/rules.d/armel.mk
@@ -7,5 +7,7 @@ build_image	= zImage
 kernel_file	= arch/$(build_arch)/boot/zImage
 install_file	= vmlinuz
 no_dumpfile = true
+# ARM is not a supported architecture in perf userspace
+do_tools	= false
 
 loader		= grub
