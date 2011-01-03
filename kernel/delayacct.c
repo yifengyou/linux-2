1064a4925050d68e9deb788d0d00025e31aa7dfa
Tim Gardner <tim.gardner@canonical.com>
2011-01-03 11:48:40 -0700

    UBUNTU: SAUCE: Change nodelayacct boot parameter polarity.
    BugLink: http://bugs.launchpad.net/bugs/493156
    
    Make the default behaviour 'delayacct_on=0' before enabling CONFIG_TASK_DELAY_ACCT.
    This preserves the released behavious of Lucid whilst still providing an avenue
    for iotop users to enable this functionality. e.g., booting with 'delayacct' on
    the grub command line.
    
    Acked-by: Brad Figg <brad.figg@canonical.com>
    Acked-by: John Johansen <john.johansen@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- kernel/delayacct.c
+++ kernel/delayacct.c
@@ -20,15 +20,15 @@
 #include <linux/sysctl.h>
 #include <linux/delayacct.h>
 
-int delayacct_on __read_mostly = 1;	/* Delay accounting turned on/off */
+int delayacct_on __read_mostly = 0;	/* Delay accounting turned on/off */
 struct kmem_cache *delayacct_cache;
 
-static int __init delayacct_setup_disable(char *str)
+static int __init delayacct_setup_enable(char *str)
 {
-	delayacct_on = 0;
+	delayacct_on = 1;
 	return 1;
 }
-__setup("nodelayacct", delayacct_setup_disable);
+__setup("delayacct", delayacct_setup_enable);
 
 void delayacct_init(void)
 {
