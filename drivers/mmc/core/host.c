a264408610c639a6fd61aa7e9d9f25d4f928d19d
Tim Gardner <tim.gardner@canonical.com>
2011-05-16 08:04:54 -0600

    Revert "mmc: fix all hangs related to mmc/sd card insert/removal during suspend/resume"
    This reverts commit 6fa73feebc2782a0eea023e2329139a882134983.
    
    Revert in favor of upstream stable which is slightly different.
    
    BugLink: http://bugs.launchpad.net/bugs/780588
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- drivers/mmc/core/host.c
+++ drivers/mmc/core/host.c
@@ -16,8 +16,6 @@
 #include <linux/idr.h>
 #include <linux/pagemap.h>
 #include <linux/leds.h>
-#include <linux/slab.h>
-#include <linux/suspend.h>
 
 #include <linux/mmc/host.h>
 
@@ -86,7 +84,6 @@ struct mmc_host *mmc_alloc_host(int extra, struct device *dev)
 	init_waitqueue_head(&host->wq);
 	INIT_DELAYED_WORK(&host->detect, mmc_rescan);
 	INIT_DELAYED_WORK_DEFERRABLE(&host->disable, mmc_host_deeper_disable);
-	host->pm_notify.notifier_call = mmc_pm_notify;
 
 	/*
 	 * By default, hosts do not support SGIO or large requests.
@@ -135,7 +132,6 @@ int mmc_add_host(struct mmc_host *host)
 #endif
 
 	mmc_start_host(host);
-	register_pm_notifier(&host->pm_notify);
 
 	return 0;
 }
@@ -152,7 +148,6 @@ EXPORT_SYMBOL(mmc_add_host);
  */
 void mmc_remove_host(struct mmc_host *host)
 {
-	unregister_pm_notifier(&host->pm_notify);
 	mmc_stop_host(host);
 
 #ifdef CONFIG_DEBUG_FS
