62f639f0fadd8c9bdb82dcbfba4ebb1c5c648c02
Tim Gardner <tim.gardner@canonical.com>
2011-09-14 10:14:17 -0600

    UBUNTU: [Config] Fix binary-% build target
    Simplify the checks-% target such that ABI and modules checks are
    built in parallel whilst both depend on the build stamp.
    
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- debian/rules.d/4-checks.mk
+++ debian/rules.d/4-checks.mk
@@ -1,25 +1,21 @@
 # Check ABI for package against last release (if not same abinum)
-$(abidir)/%: $(stampdir)/stamp-build-%
+abi-check-%: $(stampdir)/stamp-build-%
 	install -d $(abidir)
 	sed -e 's/^\(.\+\)[[:space:]]\+\(.\+\)[[:space:]]\(.\+\)$$/\3 \2 \1/'	\
-		$(builddir)/build-$*/Module.symvers | sort > $@
-
-abi-check-%: $(abidir)/%
+		$(builddir)/build-$*/Module.symvers | sort > $(abidir)/$*
 	@perl -f $(DROOT)/scripts/abi-check "$*" "$(prev_abinum)" "$(abinum)" \
 		"$(prev_abidir)" "$(abidir)" "$(skipabi)"
 
 # Check the module list against the last release (always)
-$(abidir)/%.modules: abi-check-%
+module-check-%: $(stampdir)/stamp-build-%
 	install -d $(abidir)
 	find $(builddir)/build-$*/ -name \*.ko | \
-		sed -e 's/.*\/\([^\/]*\)\.ko/\1/' | sort > $@
-
-module-check-%: $(abidir)/%.modules
+		sed -e 's/.*\/\([^\/]*\)\.ko/\1/' | sort > $(abidir)/$*.modules
 	@perl -f $(DROOT)/scripts/module-check "$*" \
 		"$(prev_abidir)" "$(abidir)" $(skipmodule)
 
-checks-%: module-check-%
-	@# Will be calling more stuff later
+checks-%: module-check-% abi-check-%
+	@echo checks-$*
 
 # Check the config against the known options list.
 config-prepare-check-%: $(stampdir)/stamp-prepare-tree-%
