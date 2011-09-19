bca1d8cd5d4ebce660604efe9f3a1974d84b7c53
Herbert Xu <herbert.xu@redhat.com>
2011-09-19 16:00:07 +0200

    core: Fix memory leak/corruption on VLAN GRO_DROP, CVE-2011-1576
    The function napi_reuse_skb is only meant to be used for packets
    merged by GRO.  Using it on the VLAN path will lead to memory
    leaks/corruption.  This patch is based on Jay Vosburgh's patch,
    and it fixes the problem by calling kfree_skb on the VLAN GRO_DROP
    path instead of napi_reuse_skb.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: Jarod Wilson <jarod@redhat.com>
    
    CVE-2011-1576
    BugLink: http://bugs.launchpad.net/bugs/844361
    (backported from kernel-2.6.18-238.19.1.el5.src.rpm)
    
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -2658,6 +2658,9 @@ gro_result_t napi_frags_finish(struct napi_struct *napi, struct sk_buff *skb,
 		break;
 
 	case GRO_DROP:
+		kfree_skb(skb);
+		break;
+
 	case GRO_MERGED_FREE:
 		napi_reuse_skb(napi, skb);
 		break;
