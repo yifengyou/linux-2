3ea32d659badf564dd4b19faae6421dc18e71f88
Theodore Ts'o <tytso@mit.edu>
2012-07-04 11:22:20 -0400

    usb: feed USB device information to the /dev/random driver
    BugLink: http://bugs.launchpad.net/bugs/1064480
    
    commit b04b3156a20d395a7faa8eed98698d1e17a36000 upstream.
    
    Send the USB device's serial, product, and manufacturer strings to the
    /dev/random driver to help seed its pools.
    
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Acked-by: Greg KH <greg@kroah.com>
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Herton Ronaldo Krzesinski <herton.krzesinski@canonical.com>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -24,6 +24,7 @@
 #include <linux/freezer.h>
 #include <linux/usb/quirks.h>
 #include <linux/rcupdate.h>
+#include <linux/random.h>
 
 #include <asm/uaccess.h>
 #include <asm/byteorder.h>
@@ -1831,6 +1832,14 @@ int usb_new_device(struct usb_device *udev)
 
 	/* Tell the world! */
 	announce_device(udev);
+
+	if (udev->serial)
+		add_device_randomness(udev->serial, strlen(udev->serial));
+	if (udev->product)
+		add_device_randomness(udev->product, strlen(udev->product));
+	if (udev->manufacturer)
+		add_device_randomness(udev->manufacturer,
+				      strlen(udev->manufacturer));
 
 	/* Register the device.  The device driver is responsible
 	 * for configuring the device and invoking the add-device
