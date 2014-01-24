70fb348592a4b104c4555534c0c24dc58a00ec52
Signed-off-by: Wu Fengguang <fengguang.wu@intel.com>
2014-01-24 14:17:22 +0000

    fanotify: FMODE_NONOTIFY and __O_SYNC in sparc conflict
    CVE-2013-0160
    
    BugLink: http://bugs.launchpad.net/bugs/1097680
    
    sparc used the same value as FMODE_NONOTIFY so change FMODE_NONOTIFY to be
    something unique.
    
    Signed-off-by: Wu Fengguang <fengguang.wu@intel.com>
    Signed-off-by: Eric Paris <eparis@redhat.com>
    (cherry picked from commit 12ed2e36c98aec6c41559222e311f4aa15d254b6)
    Signed-off-by: Luis Henriques <luis.henriques@canonical.com>
    Acked-by: Andy Whitcroft <andy.whitcroft@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -92,7 +92,7 @@ struct inodes_stat_t {
 #define FMODE_RANDOM		((__force fmode_t)4096)
 
 /* File was opened by fanotify and shouldn't generate fanotify events */
-#define FMODE_NONOTIFY		((__force fmode_t)8388608)
+#define FMODE_NONOTIFY		((__force fmode_t)16777216) /* 0x1000000 */
 
 /*
  * The below are the various read and write types that we support. Some of
